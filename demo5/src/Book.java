ce5a328297a87022ef61e0ecddd51dabb781be0a
riozi04 <thsdmswl8@naver.com>
2025-05-26 00:26:51 +0900

    Create Book.java

---
+++ demo5/src/Book.java
@@ -0,0 +1,63 @@
+package com.example.demo.domain;
+
+import jakarta.persistence.*;
+
+
+
+@Entity
+@Table(name = "book")
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 100, nullable = false)
+    private String title;
+
+    @Column(length = 50, nullable = false)
+    private String author;
+
+    @Column(nullable = false)
+    private int price;
+
+    public Book() {}
+
+    public Book(Long id, String title, String author, int price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+}
